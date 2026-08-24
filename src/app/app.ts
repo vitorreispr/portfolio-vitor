e1637c860ca4b210e5649d37142d4756ec5f74dd
vitorreispr24@gmail.com <vitorreispr24@gmail.com>
2026-08-24 09:59:59 -0300

    criando componentes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from "./menu/menu";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/menu/menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-menu',
+  styleUrl: './menu.css',
+  templateUrl: './menu.html',
+})
+export class Menu {}
